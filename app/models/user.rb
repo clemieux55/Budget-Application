528a09f40b2e8bcfcc27609d2741f114c1842f5b
Sam McTaggart <sam.mctaggart@gmail.com>
2013-07-20 21:15:36 -0400

    user model created

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+	validates_presence_of :username, null: false
+	validates_presence_of :email, null: false
+	validates_presence_of :password, null: false
+	validates_presence_of :password_confirmation, null: false
+end
