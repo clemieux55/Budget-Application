591b11f6e4d1791fe021cbdca080e8a894f3bcbc
clemieux55 <clemieux598@gmail.com>
2013-07-20 21:24:57 -0400

    user model created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+		attr_reader :username
+		
 	validates_presence_of :username, null: false
 	validates_presence_of :email, null: false
 	validates_presence_of :password, null: false
